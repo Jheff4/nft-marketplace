e466469dfb63a125d3f26341236a201f72d0537f
Jheff4 <75406244+Jheff4@users.noreply.github.com>
2022-11-07 22:22:38 +0100

    updated the nft smart contract

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -2,3 +2,11 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFT is ERC721URIStorage {
+  constructor() {
+    
+  }
+}
