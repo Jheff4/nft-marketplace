c6c43615c451acb918734277c466835d83adefc4
Jheff4 <75406244+Jheff4@users.noreply.github.com>
2022-11-07 20:10:40 +0100

    intialized the NFT smart contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+const fs = require("fs")
+const privatekey = fs.readFileSync(".secret").toString
+const projectId = "f963219dc887a30fc846e1189f0969f57cdc9ab0"
 
 module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://rpc-mumbai.maticvigil.com/v1/${projectId}`,
+      accounts: [privatekey]
+    },
+    mainnet: {
+      url: `https://rpc-mainnet.maticvigil.com/v1/${projectId}`,
+      accounts: [privatekey]
+    }
+  },
   solidity: "0.8.17",
 };
