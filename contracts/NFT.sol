c8355ca80a487bb1a905b0ffc27ac14348e6e30f
Jheff4 <75406244+Jheff4@users.noreply.github.com>
2022-11-07 23:12:47 +0100

    updated the NFT smart contract

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -6,7 +6,12 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NFT is ERC721URIStorage {
-  constructor() {
-    
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+  address contractAddress;
+
+  constructor(address marketplaceAddress) ERC721("Metaverse Tokens", "METT") {
+    contractAddress = marketplaceAddress;
   }
+  
 }
