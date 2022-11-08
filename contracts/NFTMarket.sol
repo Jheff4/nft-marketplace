1d0750b471af1640f8e40ea063bd11d07a7cd6fc
Jheff4 <75406244+Jheff4@users.noreply.github.com>
2022-11-08 22:39:49 +0100

    updated the NFT marketplace smart contract

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -5,3 +5,11 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
+contract NFTMarket is ReentrancyGuard {
+  using Counters for Counters.Counter;
+  Counters.Counter private _itemIds;
+  Counters.Counter private _itemSold;
+
+  address payable owner;
+  
+}
