e94f42b83ddeda30c4ec056a90d92a041c5746d8
Jheff4 <75406244+Jheff4@users.noreply.github.com>
2022-11-09 15:25:25 +0100

    updated the smart contract for the NFT marketplace

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -27,4 +27,28 @@ contract NFTMarket is ReentrancyGuard {
     uint256 price;
     bool sold;
   }
+
+  mapping (uint256 => MarketItem) private idToMarketItem;
+
+  event MarketItemCreated (
+    uint indexed itemId;
+    address indexed nftContract;
+    uint256 indexed tokenId;
+    address seller;
+    address owner;
+    uint256 price;
+    bool sold;
+  );
+
+  function getListingPrice() public view returns (uint256) {
+    return listingPrice;
+  }
+
+  function createMarketItem(
+    address nftContract,
+    uint256 tokenId,
+    uint256 price
+  ) public payable nonReentrant {
+    
+  }
 }
